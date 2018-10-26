ff966c027298db8cba8c074be3e76b84660bb65f
Krister <krister@pixeltokig.se>
2018-10-26 13:19:07 +0200

    plopp

---
+++ wordpress/user access and capabilities/user-access.php
@@ -0,0 +1,22 @@
+<?php
+
+/*--------------------------------------------------------------
+	Block access to wp-admin for specific roles
+--------------------------------------------------------------*/
+// authors and above has the capability to publish_posts, everyone else gets redirected
+// admin_init is triggered before any other hook when a user accesses the admin area.
+     
+add_action( 'admin_init', 'blockusers_init' );
+function blockusers_init() {
+	if (
+		is_admin() &&
+		!current_user_can( 'publish_posts' ) &&
+		!( defined( 'DOING_AJAX' ) && DOING_AJAX )
+	) {
+		wp_redirect( home_url() );
+		exit;
+	}
+}
+
+
+?>
